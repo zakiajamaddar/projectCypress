49a725ce74c29dbd0ef36fca1db21e091720ca45
zakia-sultana <zakiajamaddar@gmail.com>
2023-01-10 11:10:33 -0500

    first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe("My First Test Suite", () => {
+  it("My FirstTest case", () => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length',4)
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+    cy.get('.products').find('.product').each(($el,index,$list) => {
+    const textVeg =  $el.find('h4.product-name').text() 
+    if (textVeg.includes('Cashews'))
+    {
+        cy.wrap($el).find('button').click()
+    }
+    })
+  });
+});
