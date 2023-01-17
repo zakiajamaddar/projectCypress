50e8e36de63b2a1e61e90f20636d7372cb336fff
zakia-sultana <zakiajamaddar@gmail.com>
2023-01-17 12:02:14 -0500

    second commit

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,14 +6,21 @@ describe("My First Test Suite", () => {
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
     cy.get('.product:visible').should('have.length',4)
-    cy.get('.products').find('.product').should('have.length',4)
-    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
-    cy.get('.products').find('.product').each(($el,index,$list) => {
+    cy.get('.products').as('productLocator')
+    cy.get('@productLocator').find('.product').should('have.length',4)
+    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+    cy.get('@productLocator').find('.product').each(($el,index,$list) => {
     const textVeg =  $el.find('h4.product-name').text() 
     if (textVeg.includes('Cashews'))
     {
         cy.wrap($el).find('button').click()
     }
+    })
+
+    cy.get(".brand").should('have.text','GREENKART')
+    cy.get('.brand').then(function(logoElement){
+    
+      cy.log(logoElement.text())
     })
   });
 });
